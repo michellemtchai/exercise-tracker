d48b3c0ebb1f8162095c9ae56458d172ac3bb16e
Michelle Chai <michellemtchai@gmail.com>
2024-08-25 03:40:07 -0400

    update packages

--- server.js
+++ server.js
@@ -1,48 +1,47 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const routes = require('./routes');
 app.use('/api', routes);
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
-
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+  return next({ status: 404, message: 'not found' });
+});
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
-  let errCode, errMessage
+  let errCode, errMessage;
 
   if (err.errors) {
     // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
     // report the first validation error
-    errMessage = err.errors[keys[0]].message
+    errMessage = err.errors[keys[0]].message;
   } else {
     // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
   }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
+  res.status(errCode).type('txt').send(errMessage);
+});
 
 var port = process.env.PORT || 3000;
 const listener = app.listen(port, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
